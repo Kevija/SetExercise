c96c4db247decdb598dfff00a8ad0fcaffb76e6c
ek <evija.kvante@inbox.lv>
2022-03-28 21:05:40 +0300

    First Commit..

---
+++ src/main/scala/SetExercise.scala
@@ -0,0 +1,25 @@
+object SetExercise extends App {
+
+  // write a pangram tester
+  //https://en.wikipedia.org/wiki/Pangram
+  //check if text contains every letter of an alphabet at least once
+  //for this task consider upuper and lower case the same
+  def isPangram(text:String, alpha:String="abcdefghijklmnopqrstuvwxyz"):Boolean = {
+
+    val myTextSet = text.toLowerCase().toSet
+println(myTextSet)
+
+    val alphaSet = alpha.toSet
+    println(alphaSet)
+
+    val check = myTextSet & alphaSet
+    check.subsetOf(alphaSet)
+
+  }
+
+  val myTestText = "The five boxing wizards jump quickly."
+  println(isPangram(myTestText)) //should be true
+
+}
+
+
